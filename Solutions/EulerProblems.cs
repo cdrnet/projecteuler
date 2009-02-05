4f5a5d431d28d5740c7ce39d75caee9cccb9e92b
Christoph Ruegg <git@cdrnet.ch>
2009-02-05 21:36:43 +0100

    P8: major speedup due to eliminated string parsing

--- Solutions/EulerProblems.cs
+++ Solutions/EulerProblems.cs
@@ -244,35 +244,39 @@ namespace ProjectEuler.Solutions
                 {
                     new Approach
                     {
+                        WarmupRounds = 5,
+                        BenchmarkRounds = 500,
                         Algorithm = delegate
                         {
                             string number = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-                            double product = 0;
-                            double temp = 1;
-                            int count = -1;
+                            int max = 0;
+                            const int zero = (int)'0';
 
                             int[] consecutiveValues = new int[5];
-                            consecutiveValues[0] = Convert.ToInt32(number[0].ToString());
-                            consecutiveValues[1] = Convert.ToInt32(number[1].ToString());
-                            consecutiveValues[2] = Convert.ToInt32(number[2].ToString());
-                            consecutiveValues[3] = Convert.ToInt32(number[3].ToString());
-                            consecutiveValues[4] = Convert.ToInt32(number[4].ToString());
+                            for(int i=0;i<consecutiveValues.Length;i++)
+                            {
+                                consecutiveValues[i] = (int)number[i] - zero;
+                            }
 
                             for (int i = 4; i < number.Length; i++)
                             {
-                                if (count >= 0)
-                                    consecutiveValues[count % 5] = Convert.ToInt32(number[i].ToString());
+                                consecutiveValues[i % 5] = (int)number[i] - zero;
 
-                                foreach (int num in consecutiveValues)
-                                    temp *= num;
-                                if (temp > product)
-                                    product = temp;
+                                int product = consecutiveValues[0];
+                                for(int j=1; j<consecutiveValues.Length; j++)
+                                {
+                                    // Note, reusing the product is possible, 
+                                    // but requires a division and special case for 0.
+                                    product *= consecutiveValues[j];
+                                }
 
-                                count++;
-                                temp = 1;
+                                if (product > max)
+                                {
+                                    max = product;
+                                }
                             }
 
-                            return Convert.ToInt64(product);
+                            return max;
                         }
                     }
                 },
