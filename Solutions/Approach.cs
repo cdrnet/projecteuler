e5333164044b5c026e0743c68d99ede411b50c94
Christoph Ruegg <git@cdrnet.ch>
2009-02-05 18:58:00 +0100

    major refactoring

---
+++ Solutions/Approach.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProjectEuler.Solutions
+{
+    public class Approach
+    {
+        public string Title { get; set; }
+        public Func<long> Algorithm { get; set; }
+        public int WarmupRounds { get; set; }
+        public int BenchmarkRounds { get; set; }
+
+        public Approach()
+        {
+            Title = "Unnamed";
+            WarmupRounds = 1;
+            BenchmarkRounds = 5;
+        }
+
+        public void Run(Stopwatch watch)
+        {
+            Run(watch, WarmupRounds, BenchmarkRounds);
+        }
+
+        public void Run(Stopwatch watch, int warmupRounds, int benchmarkRounds)
+        {
+            Func<long> algorithm = Algorithm;
+            long result = 0;
+
+            // dummy variable trying to ensure nothing is optimized away.
+            long dummy = long.MaxValue;
+
+            // JIT/Warmup
+            for(int i = 0; i < warmupRounds; i++)
+            {
+                result = algorithm();
+                dummy ^= result;
+            }
+
+            // Try to prevent GC run during execution
+            GC.Collect();
+            GC.Collect();
+
+            // Benchmark
+            watch.Reset();
+            watch.Start();
+            for(int i = 0; i < benchmarkRounds; i++)
+            {
+                result = algorithm();
+                dummy ^= result;
+            }
+            watch.Stop();
+
+            Console.WriteLine("{0}: {1}", Title, result);
+            Console.Write("Elapsed Time: ");
+            ConsoleColor previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("{0} ms ({1} ticks)", watch.ElapsedMilliseconds / benchmarkRounds, watch.ElapsedTicks / benchmarkRounds);
+            Console.ForegroundColor = previousColor;
+            Console.WriteLine(" on {0} rounds average. D={1}", benchmarkRounds, dummy & 0x1);
+            Console.WriteLine();
+        }
+    }
+}
--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -7,126 +7,81 @@ namespace ProjectEuler.Solutions
 {
     static class Program
     {
-        private static bool _exit = false;
+        static EulerProblems _problems = new EulerProblems();
+        static bool _exit = false;
 
         static void Main(string[] args)
         {
-            _mainMenu("Welcome!");
+            MenuLoop();
         }
 
-        private static void _mainMenu(string mes)
+        static void MenuLoop()
         {
-            string message = mes;
-            while (!_exit)
+            Console.Clear();
+            Console.WriteLine("Welcome!");
+
+            while(true)
             {
-                Console.Clear();
-
-                if (!string.IsNullOrEmpty(message))
-                    Console.WriteLine(message);
-
                 Console.WriteLine("What question would you like to run?");
+
                 string selection = Console.ReadLine();
                 Console.WriteLine();
 
-                message = _switchBoard(selection);
+                switch (selection.ToUpper())
+                {
+                    case "EXIT":
+                    case "E":
+                    case "QUIT":
+                    case "Q":
+                    case "":
+                        return;
+
+                    case "HELP":
+                    case "H":
+                    case "?":
+                        Help();
+                        break;
+
+                    case "LIST":
+                    case "L":
+                        List();
+                        break;
+
+                    case "TEST":
+                    case "T":
+                        Tests.GetHashCodes_Test();
+                        break;
+
+                    default:
+                        RunProblem(selection);
+                        break;
+                }
+
+                Console.ReadLine();
+                Console.Clear();
             }
         }
-        private static string _switchBoard(string selection)
-        {
-            bool isKeyword = true;
-            switch (selection.ToUpper())
-            {
-                case "?":
-                    break;
-                case "LIST":
-                case "L":
-                    break;
-                case "":
-                case "EXIT":
-                case "E":
-                    _exit = true;
-                    break;
-                case "TEST":
-                case "T":
-                    Tests.GetHashCodes_Test();
-                    break;
-                default:
-                    isKeyword = false;
-                    break;
-            }
 
-            if (!isKeyword)
-            {
-                try
-                {
-                    int question = Convert.ToInt32(selection);
-                    return _runQuestion(question);
-                }
-                catch (FormatException ex)
-                {
-                    //_mainMenu(string.Format("[{0}] is an invalid selection.", selection));'
-                    return string.Format("[{0}] is an invalid selection.", selection);
-                }
-            }
-            return string.Empty;
+        static void Help()
+        {
+            Console.WriteLine("Help not available yet.");
         }
-        private static string _runQuestion(int question)
-        {
-            string message = string.Empty;
-            switch (question)
-            {
-                case 1:
-                    Solution.Problem1();
-                    break;
-                case 2:
-                    Solution.Problem2();
-                    break;
-                case 3:
-                    Solution.Problem3();
-                    break;
-                case 4:
-                    Solution.Problem4();
-                    break;
-                case 5:
-                    Solution.Problem5();
-                    break;
-                case 6:
-                    Solution.Problem6();
-                    break;
-                case 7:
-                    Solution.Problem7();
-                    break;
-                case 8:
-                    Solution.Problem8();
-                    break;
-                case 9:
-                    Solution.Problem9();
-                    break;
-                case 10:
-                    Solution.Problem10();
-                    break;
-                case 11:
-                    Solution.Problem11();
-                    break;
-                case 12:
-                    Solution.Problem12();
-                    break;
-                case 13:
-                    Solution.Problem13();
-                    break;
-                case 14:
-                    Solution.Problem14();
-                    break;
-                case 16:
-                    Solution.Problem16();
-                    break;
 
-                default:
-                    //_mainMenu(string.Format("I'm sorry, but question {0} hasn't been answered yet.", question.ToString()));
-                    message = string.Format("I'm sorry, but question {0} hasn't been answered yet.", question.ToString());
-                    break;
+        static void List()
+        {
+            Console.WriteLine("List not available yet.");
+        }
+
+        static void RunProblem(string selection)
+        {
+            int questionId;
+            if(!Int32.TryParse(selection, out questionId))
+            {
+                Console.WriteLine("[{0}] is an invalid selection.", selection);
+                return;
             }
-            return message;
+
+            _problems.RunProblem(questionId);
         }
     }
 }
